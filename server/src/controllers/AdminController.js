8941a78e1d2cd75cb3076e2a43f8dfe467f899a8
bbrillianta <brilliantabintangvirgantara@gmail.com>
2021-06-15 08:16:23 +0700

    insyaallah done

--- server/src/controllers/AdminController.js
+++ server/src/controllers/AdminController.js
@@ -1,3 +1,19 @@
 module.exports = class AdminController {
-    
+    #adminService;
+
+    constructor(adminService) {
+        this.#adminService = adminService;
+    }
+
+    async editTransactionVerification(req, res, next) {
+        const updatedTransaction = await this.#adminService.verifyPayment(req.body);
+
+        res.json(updatedTransaction);
+    }
+
+    async deleteTransaction(req, res, next) {
+        const deletedTransaction = await this.#adminService.invalidPayment(req.body);
+
+        res.json(deletedTransaction);
+    }
 }
--- server/src/routes/admin.js
+++ server/src/routes/admin.js
@@ -1,9 +1,18 @@
 const express = require('express');
 
+const AdminController = require('../controllers/AdminController');
+const Transaction = require('../models/Transaction');
+const User = require('../models/User');
+const AdminService = require('../services/AdminService');
+
 const router = express.Router();
+const adminService = new AdminService(User, Transaction);
+const adminController = new AdminController(adminService);
 
 module.exports = (app) => {
-    app.use('/admin');
+    app.use('/admin', router);
 
-    
+    router.put('/transaction/verify', async (req, res, next) => await adminController.editTransactionVerification(req, res, next));
+
+    router.delete('/transaction/delete', async (req, res, next) => await adminController.deleteTransaction(req, res, next));
 }
